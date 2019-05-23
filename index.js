6a431969406f7ac23d1b4951159d75ff986cfbde
samalas <sachit478@gmail.com>
2019-05-22 19:56:02 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Route, Link, BrowserRouter as Router, Switch } from "react-router-dom";
+import App from "./components/App";
+
+import Dashboard from "./components/Dashboard";
+import "../style/reset.css";
+import "../style/main.scss";
+
+const routing = (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={LoginForm} />
+      <Route exact path="/" component={Dashboard} />
+      <Route path="/select" component={Select} />
+      <Route path="/case" component={Case} />
+      <Route path="/addclient" component={AddClientForm} />
+      <Route path="/findlawyer" component={AddServiceForm} />
+      <Route path="/recommend" component={LawyerRecommend} />
+      <Route path="/confirm" component={Confirmation} />
+      <Route path="/searchClient" component={SearchClient} />
+      <Route path="/dashboard" component={Dashboard} />
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById("app"));
